a4b9250e09fdd28728ddbcae92d84f497f8ba7ac
Pawan Siriwardena <90904204+pawansiri@users.noreply.github.com>
2022-10-10 02:47:48 +0530

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+This project is a Student Management System develped using ASP.Net MVC.
---
+++ Student_Management_System/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using Student_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        StudentManageEntities db = new StudentManageEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(User objcheck)
+        {
+            if(ModelState.IsValid)
+            {
+                using (StudentManageEntities db = new StudentManageEntities())
+                {
+
+                    var obj = db.Users.Where(a => a.Username.Equals(objcheck.Username) && a.Password.Equals(objcheck.Password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserID.ToString();
+                        Session["Username"] = obj.Username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The Username or Password is Incorrect, Please try again");
+
+
+
+                    }
+
+                }
+            }
+
+           
+            return View(objcheck);
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
